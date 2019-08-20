e645ffbf87469d121e24f795b55b428aa9f7286c
railken <railken@protonmail.ch>
2019-08-21 00:02:31 +0200

    bye bye quartz

--- stubs/quartz-data/src/app/Api/__--EntityName__classify--__Api.js
+++
@@ -1,5 +0,0 @@
-import { ResourceApi } from '@railken/quartz-core'
-
-export class {{ EntityName|classify }}Api extends ResourceApi {
-    resource_url = '/admin/{{ EntityName|kebab|pluralize }}';
-};
--- stubs/quartz-data/src/components/__--EntityName__kebab--__/Resource/create.vue
+++
@@ -1,7 +0,0 @@
-<template>
-  <q-resource-create v-bind="$attrs">
-    <template slot='create' slot-scope="scope">
-      <q-text v-model="scope.resource" :attribute="scope.config.getAttribute('name')" :errors="scope.errors"/>
-    </template>
-  </q-resource-create>
-</template>
